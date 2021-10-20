3c9759f0c6e782460b97aa12b482d4a7f7cb3430
957283329 <39431859+957283329@users.noreply.github.com>
2021-10-20 16:13:37 +0800

    pull test

--- src/test/java/com/zwq/git/gitTest.java
+++ src/test/java/com/zwq/git/gitTest.java
@@ -10,5 +10,6 @@ public class gitTest {
         System.out.println("冲突测试 hot-fix");
         System.out.println("push test");
         System.out.println("push test2");
+        System.out.println("pull test");
     }
 }
